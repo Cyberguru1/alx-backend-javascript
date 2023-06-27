9bee3e667436374454481927e572004e374922a6
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-26 23:31:00 -0500

    new update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,15 @@
+/* eslint-disable prefer-const */
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-  
-    if (trueOrFalse) {
-      /* eslint-disable no-unused-vars */
-      const task = true;
-      const task2 = false;
-      /* eslint-disable no-unused-vars */
-    }
-  
-    return [task, task2];
+  // eslint-disable-next-line prefer-const
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
+    /* eslint-disable no-unused-vars */
   }
+
+  return [task, task2];
+}
