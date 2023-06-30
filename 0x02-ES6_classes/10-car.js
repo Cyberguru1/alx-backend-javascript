cd57af94319d0fe2d0557f461b593303713db8a6
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-30 12:04:51 -0500

    new update

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,9 +6,9 @@ export default class Car {
   }
   get brand() {
     return this._brand;
-  }    set brand(brand) {
-  this._brand = brand;
-
+  }    
+  set brand(brand) {
+    this._brand = brand;
   }
   get motor() {
     return this._motor;
