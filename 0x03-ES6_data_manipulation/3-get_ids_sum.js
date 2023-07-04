d7f5058b029df863f464771fbe3a9597ae0bda32
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-04 18:50:10 -0500

    task 1

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 // function that returns the sum of all students
 
-export function getStudentIdsSum(students){
+function getStudentIdsSum(students){
   return students.reduce((t, x) => t + x.id, 0);  
 }
 
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,11 @@
 // function that returns a new ArrayBuffer with an Int8
 
-export function createInt8TypedArray(length, position, value){
-  
-}
+function createInt8TypedArray(length, position, value){
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  if (position > int8Array.length) throw new Error('Position outside range');
+    int8Array[position] = value;
+    return new DataView(buffer);
+};
+      
+export default createInt8TypedArray;
