e9a7d09232be518f95130d525695b9f3be48705d
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-04 18:45:26 -0500

    task 1

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
 // function that retuns an array of objects
 
 export function getStudentsByLocation(students, city){
-  return students.filter((x) => x.location === city);
+  return students.filter((x) => x.location == city);
 }
 
 export default getListStudentsByLocation;
