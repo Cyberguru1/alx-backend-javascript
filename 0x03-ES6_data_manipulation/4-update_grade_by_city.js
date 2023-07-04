ada80fab99b039cde7c7f156183bdcff42820714
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-04 17:34:36 -0500

    task 1

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,19 @@
+// function that returns an array of students for 
+// a specific city
+
+export function updateStudentGradeByCity(students, city, newGrades){
+  filterd_list = students.filter((x) => x.city == city);
+  return filterd_list.map((stud) => {
+    const filteredGrade = newGrades.filter((grade) => grade.studentId == stud.Id);
+    if (filteredGrade.length > 0) {
+      return {
+        ...stud,
+        grade:filteredGrade[0]['grade']
+      };
+    }
+    return {
+        ...stud,
+        grade: "N/A"
+      };
+  });
+}
