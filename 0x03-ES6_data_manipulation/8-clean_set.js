cc7dfe377042eafd267c0a0ec39b7aab2e25ef70
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-04 19:06:05 -0500

    task 1

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+// function to perform
+
+cleanSet = (set, starts) => {
+  const str = [];
+  if (starts === '' || typeof starts !== 'string') return '';
+  set.ForEach((s) => {
+    if (typeof s === 'string' && s.startsWith(starts)) {
+      str.push(s.slice(starts.length));
+    }
+  });
+	return str.join('-')
+}
+
+export default cleanSet;
