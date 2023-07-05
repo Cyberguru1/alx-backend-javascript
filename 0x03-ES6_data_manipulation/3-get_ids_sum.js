f732344a150e0cbead2303f2c6334581abbffe6c
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-05 02:24:05 -0500

    test cases

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 // function that returns the sum of all students
 
-function getStudentIdsSum(students){
-  return students.reduce((t, x) => t + x.id, 0);  
+function getStudentIdsSum(students) {
+  return students.reduce((t, x) => t + x.id, 0);
 }
 
 export default getStudentIdsSum;
