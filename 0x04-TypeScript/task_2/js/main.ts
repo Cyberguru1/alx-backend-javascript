77b285880ff9fcd9de139a7e67b0a132ac0a7a7d
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-05 07:35:43 -0500

    test cases

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,50 +1,84 @@
-class Teacher {
-  firstName: string;
-  lastName: string;
-  fullTimeEmployee: boolean
-  location: string
-  yearsOfExperience?: number
-  [key: string]: any
+interface DirectorInterface {
+  workFromHome: () => string;
+  getCoffeBreak: () => string;
+  workDirectorTasks: () => string
+}
 
-  constructor(
-    firstName: string,
-    lastName: string, 
-    fullTimeEmployee: boolean,
-    location: string,
-    yearsOfExperience? : number,
-    ) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.fullTimeEmployee = fullTimeEmployee;
-    this.yearsOfExperience = yearsOfExperience;
-    this.location = location;
 
+interface TeacherInterface {
+  workFromHome: () => string;
+  getCoffeBreak: () => string;
+  workTeacherTasks: () => string
+}
+
+class Director implements DirectorInterface {
+
+  workFromHome() {
+    return 'Working from home'
   }
+
+  getCoffeBreak() {
+    return "Getting a coffee break"
+  }
+
+  workDirectorTasks = () => {return "Getting to director tasks"}
 }
 
-interface Directory extends Teacher {
-  numberOfReports: number;
+class Teacher implements TeacherInterface {
+
+  workFromHome() {
+    return 'Cannot work from home'
+  }
+
+  getCoffeBreak() {
+    return "Cannot have a break"
+  }
+
+  workTeacherTasks = () => {return "Getting to work"}
 }
 
-const t1: Directory = {
-  firstName: "Joe",
-  lastName: "biden",
-  fullTimeEmployee: false,
-  location: "France",
-  name: "hamza",
-  numberOfReports: 24,
+const createEmployee = (obj: number | string ):  Director | Teacher => {
+  if ((typeof obj === "number") && (obj < 500)) {
+    return new Teacher;
+  }
+  return new Director;
 }
 
-console.log(t1)
+console.log(createEmployee(200));
+// output
+// Teacher
+console.log(createEmployee(1000));
+// output
+// Director
+console.log(createEmployee('$500'));
+// output
+// Directory
 
-// prints
-// {
-//   firstName: 'Joe',
-//   lastName: 'biden',
-//   fullTimeEmployee: false,
-//   location: 'France',
-//   name: 'hamza',
-//   numberOfReports: 24
-// }
+// task 6
 
+function isDirector(employee: DirectorInterface | TeacherInterface) {
+  return employee instanceof Director;
+}
 
+function executeWork(obj: any) {
+  if (isDirector(obj)) {
+  return obj.workDirectorTasks();
+  }
+  return obj.workTeacherTasks();
+}
+
+console.log(executeWork(createEmployee(200)));
+// output
+// Getting to work
+
+console.log(executeWork(createEmployee(1000)));
+// output
+// Getting to director tasks
+
+// task 7
+
+type Subject = "math" | "Histroy";
+
+const teachClass(todayClass: Subject): string => {
+  if (typeof)
+}
